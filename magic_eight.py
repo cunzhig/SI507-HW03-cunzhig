c46e5c798cefbec61d0e72a9cba20ca39ce2791f
hhw98 <hhw98@mail.missouri.edu>
2018-09-19 11:55:01 -0400

    final_add_function

--- magic_eight.py
+++ magic_eight.py
@@ -26,27 +26,62 @@ def add_questions():
         print "It is certain"
 
     elif answers == 2:
-        print "Outlook good"
+        print "It is decidedly so."
 
     elif answers == 3:
-        print "You may rely on it"
+        print "Without a doubt."
 
     elif answers == 4:
-        print "Ask again later"
+        print "Yes - definitely."
 
     elif answers == 5:
-        print "Concentrate and ask again"
+        print "You may rely on it."
 
     elif answers == 6:
-        print "Reply hazy, try again"
+        print "As I see it, yes"
 
     elif answers == 7:
-        print "My reply is no"
+        print "Most likely."
 
     elif answers == 8:
-        print "My sources say no"
+        print "Outlook good."
+
     elif answers == 9:
-        print "NO"
+        print " Yes."
+
+    elif answers == 10:
+        print "Signs point to yes."
+
+    elif answers == 11:
+        print " Reply hazy, try again"
+
+    elif answers == 12:
+        print "Ask again later."
+
+    elif answers == 13:
+        print "Better not tell you now."
+
+    elif answers == 14:
+        print "Cannot predict now."
+
+    elif answers == 15:
+        print "Concentrate and ask again."
+
+    elif answers == 16:
+        print "Don't count on it."
+
+    elif answers == 17:
+        print "My reply is no."
+
+    elif answers == 18:
+        print "My sources say no."
+
+    elif answers == 19:
+        print "Outlook not so good."
+
+    elif answers == 20:
+        print "Very doubtful."
+
 
 	# Program start
 def start():
@@ -54,3 +89,4 @@ def start():
         question = ask_question()
         if check_question(question):
             break
+    add_answers()
