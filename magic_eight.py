0a2229fed2c6491170aa804b0d5619ec331ca513
hhw98 <hhw98@mail.missouri.edu>
2018-09-19 10:55:58 -0400

    add questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,38 @@
+import sys
+import random
 def ask_question():
     print("What is your question?")
+
+def ask_question():
+    print("What is your question?")
+
+def add_questions():
+
+    answers = random.randint(1,8)
+    
+    if question == "":
+        sys.exit()
+    
+    elif answers == 1:
+        print "It is certain"
+    
+    elif answers == 2:
+        print "Outlook good"
+    
+    elif answers == 3:
+        print "You may rely on it"
+    
+    elif answers == 4:
+        print "Ask again later"
+    
+    elif answers == 5:
+        print "Concentrate and ask again"
+    
+    elif answers == 6:
+        print "Reply hazy, try again"
+    
+    elif answers == 7:
+        print "My reply is no"
+    
+    elif answers == 8:
+        print "My sources say no"	
