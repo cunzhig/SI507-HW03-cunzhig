9eb268ca870a8509e81c17f5a38933c2c2c5d043
root <gaocunzhi@sjtu.edu.cn>
2018-09-19 10:54:57 -0400

    check question

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,27 @@
+# Ask user to input a question
 def ask_question():
-    print("What is your question?")
+    question = input("What is your question?")
+    return question
+
+# Check wether the input is a question
+# param: question - string, input of the user
+def check_question(question):
+    if question.endswith("?"):
+        return True
+    else:
+        print("I’m sorry, I can only answer questions.")
+        return False
+
+
+
+
+# Program start
+def start():
+    while True:
+        question = ask_question()
+        if check_question(question):
+            break
+
+
+
+
